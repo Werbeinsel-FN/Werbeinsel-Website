04ecce307d2ce15d8a42b5c36ff60beadce541b3
SamTBM <Samy.Mettenmeyer@gmx.net>
2025-06-14 12:38:00 +0200

    added initial contact template

---
+++ wp-content/themes/wi-theme/templates/contact-template.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Template Name: Kontakt
+ * Description: Template for the contact page
+ */
+
+get_header(); ?>
+
+<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/contact.css?v=<?php echo filemtime(get_template_directory() . '/css/home.css'); ?>">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+<main id="contact-main" class="contact-main">
+    <div class="contact-main-container">
+    </div>
+</main>
+
+<?php get_footer();
