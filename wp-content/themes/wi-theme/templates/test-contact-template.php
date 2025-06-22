3b9b6f983b69e8c239654418604c10d278494096
SamTBM <Samy.Mettenmeyer@gmx.net>
2025-06-22 15:21:07 +0200

    included test-contact-css

--- wp-content/themes/wi-theme/templates/test-contact-template.php
+++ wp-content/themes/wi-theme/templates/test-contact-template.php
@@ -6,11 +6,11 @@
 
 get_header(); ?>
 
-<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/contact.css?v=<?php echo filemtime(get_template_directory() . '/css/home.css'); ?>">
+<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/test-contact.css?v=<?php echo filemtime(get_template_directory() . '/css/home.css'); ?>">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/foundation-sites@6.7.5/dist/css/foundation.min.css">
-<link href="https://fonts.googleapis.com/css2?family=Unbounded:wght@400;700;800&display=swap" rel="stylesheet">
-<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700;800&display=swap" rel="stylesheet">
+<!-- <link href="https://fonts.googleapis.com/css2?family=Unbounded:wght@400;700;800&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700;800&display=swap" rel="stylesheet"> -->
 
 <main id="test-contact-main" class="test-contact-main">
     <h3 class="contact-main-container headline">Kontakt</h3>
