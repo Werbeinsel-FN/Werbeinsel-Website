8916fb875f006d87078956b4533b7b4ddc9bb4a5
SamTBM <Samy.Mettenmeyer@gmx.net>
2025-06-22 15:18:23 +0200

    added test template

---
+++ wp-content/themes/wi-theme/templates/test-contact-template.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Template Name: Test Kontakt
+ * Description: Template for the contact page
+ */
+
+get_header(); ?>
+
+<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/contact.css?v=<?php echo filemtime(get_template_directory() . '/css/home.css'); ?>">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/foundation-sites@6.7.5/dist/css/foundation.min.css">
+<link href="https://fonts.googleapis.com/css2?family=Unbounded:wght@400;700;800&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700;800&display=swap" rel="stylesheet">
+
+<main id="test-contact-main" class="test-contact-main">
+    <h3 class="contact-main-container headline">Kontakt</h3>
+    <?php echo do_shortcode('[wi_form id="30"]'); ?>
+</main>
+
+<?php get_footer();
