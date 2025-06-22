755af1382517542c6533dd41c9e2f0bfbb31acaa
SamTBM <Samy.Mettenmeyer@gmx.net>
2025-06-22 21:51:05 +0200

    css

--- wp-content/themes/wi-theme/templates/test-contact-template.php
+++ wp-content/themes/wi-theme/templates/test-contact-template.php
@@ -6,15 +6,29 @@
 
 get_header(); ?>
 
-<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/contact.css?v=<?php echo filemtime(get_template_directory() . '/css/home.css'); ?>">
+<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/test-contact.css?v=<?php echo filemtime(get_template_directory() . '/css/test-contact.css'); ?>">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/foundation-sites@6.7.5/dist/css/foundation.min.css">
 <link href="https://fonts.googleapis.com/css2?family=Unbounded:wght@400;700;800&display=swap" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700;800&display=swap" rel="stylesheet">
 
-<main id="test-contact-main" class="test-contact-main">
-    <h3 class="contact-main-container headline">Kontakt</h3>
-    <?php echo do_shortcode('[wi_form id="30"]'); ?>
+<main id="wi-contact-main" class="wi-contact-main">
+    <!-- <h3 class="contact-main-container headline">Kontakt</h3>
+    <?php echo do_shortcode('[wi_form id="30"]'); ?> -->
+
+    <div id="contact-form" class="content-form contact-form">
+	
+	<div class="content row loaded">
+	
+		<h3 class="contact-form headline text-center">
+            Kontakt</h3>
+		<div class="contact-form subline text-center">
+            Einfach das Formular ausfüllen - wir freuen uns auf Sie!
+        </div>
+
+        <?php echo do_shortcode('[wi_form id="30"]'); ?> 
+	</div>
+</div>
 </main>
 
 <?php get_footer();
