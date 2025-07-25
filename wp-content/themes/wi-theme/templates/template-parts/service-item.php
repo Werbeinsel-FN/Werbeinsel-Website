a6c90aa78d6f9c50596abda47f5e3839b6d26a43
SamTBM <Samy.Mettenmeyer@gmx.net>
2025-07-25 21:18:44 +0200

    2nd cleanup (finished services desktop)

--- wp-content/themes/wi-theme/templates/template-parts/service-item.php
+++ wp-content/themes/wi-theme/templates/template-parts/service-item.php
@@ -10,24 +10,20 @@
 ?>
 
 <article class="service-item" itemscope itemtype="https://schema.org/Service">
-    <div class="service-thumbnail">
-        <a href="<?= get_permalink($service); ?>" itemprop="url">
-            <?= get_the_post_thumbnail($service, 'full', ['itemprop' => 'image']); ?>
-            <div class="service-overlay">
-                <div class="service-overlay-content">
-                    <div class="service-subtitle">
-                        <?php
-                        $terms = get_the_terms($service->ID, 'service_category');
-                        if (!empty($terms) && !is_wp_error($terms)) :
-                            echo '<h3 itemprop="description">' . esc_html($terms[0]->name) . '</h3>';
-                        endif;
-                        ?>
-                    </div>
-                    <div class="service-title">
-                        <h2 itemprop="name"><?= get_the_title($service); ?></h2>
-                    </div>
-                </div>
+    <a href="<?= get_permalink($service); ?>" itemprop="url">
+        <?= get_the_post_thumbnail($service, 'full', ['itemprop' => 'image']); ?>
+        <div class="service-overlay">
+            <div class="service-subtitle">
+                <?php
+                $terms = get_the_terms($service->ID, 'service_category');
+                if (!empty($terms) && !is_wp_error($terms)) :
+                    echo '<h3 itemprop="description">' . esc_html($terms[0]->name) . '</h3>';
+                endif;
+                ?>
             </div>
-        </a>
-    </div>
+            <div class="service-title">
+                <h2 itemprop="name"><?= get_the_title($service); ?></h2>
+            </div>
+        </div>
+    </a>
 </article>
