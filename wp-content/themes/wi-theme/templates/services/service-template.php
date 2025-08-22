d9fc81b3e8701810d24bb3ee760e8248aa3f1e42
SamTBM <Samy.Mettenmeyer@gmx.net>
2025-08-22 15:19:23 +0200

    added elementor content call

--- wp-content/themes/wi-theme/templates/services/service-template.php
+++ wp-content/themes/wi-theme/templates/services/service-template.php
@@ -1,39 +1,37 @@
 <?php
 /* Template Name: Single Service */
 
-get_header(); // Lade den Header
+get_header();
 
-if (have_posts()) : while (have_posts()) : the_post();
+if (have_posts()) :
+  while (have_posts()) : the_post();
 ?>
 
 <main id="service-main" class="service-main">
-	<div class="inner-wrapper">
-		<header class="service-title-wrapper">
-			<h1 class="service-title">
-				<?php the_title(); ?>
-			</h1>
-		</header>	
-		<div class="service-content-wrapper">
-			<div class="quote-wrapper">
-				<div class="msg-wrapper">
-					"Plakatieren ist meine Leidenschaft, die mir Spaß und Freude macht."
-				</div>
-				<div class="author-wrapper">
-					(Maurizio Lakovara, Plakatierer)
-				</div>
-			</div>
-			<div class="service-thumbnail-wrapper">
-				<?php
-				if (has_post_thumbnail()) {
-					$full_image_url = get_the_post_thumbnail_url(get_the_ID(), 'full');
-					echo '<img src="' . esc_url($full_image_url) . '" alt="">';
-				}
-				?>			
-			</div>
-		</div>
-	<div>
+  <div class="inner-wrapper">
+    <header class="service-title-wrapper">
+      <h1 class="service-title"><?php the_title(); ?></h1>
+    </header>
+
+    <div class="service-content-wrapper">
+      <div class="service-thumbnail-wrapper">
+        <?php
+        if (has_post_thumbnail()) {
+          $full_image_url = get_the_post_thumbnail_url(get_the_ID(), 'full');
+          echo '<img src="' . esc_url($full_image_url) . '" alt="">';
+        }
+        ?>
+      </div>
+
+      <div class="service-editor-content">
+        <?php the_content(); // <-- Call Elementor Content ?>
+      </div>
+    </div>
+  </div>
 </main>
 
-<?php 
-endwhile; endif;
-get_footer(); // Lade den Footer
+<?php
+  endwhile;
+endif;
+
+get_footer();
