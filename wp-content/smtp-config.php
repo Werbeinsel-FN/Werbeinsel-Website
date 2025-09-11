8e8adb8c1d0a33c0259e5862736465f64533a16d
marko <marko3ecvetkovic@gmail.com>
2025-09-11 21:33:59 +0200

    SMTP: update password for IONOS2

--- wp-content/smtp-config.php
+++ wp-content/smtp-config.php
@@ -1,10 +1,10 @@
 <?php
 // === SMTP / Mailbox konfiguracija ===
 define('SMTP_HOST', 'smtp.ionos.de');  // ili smtp.strato.de / email-smtp.<region>.amazonaws.com ...
-define('SMTP_PORT', 465);
-define('SMTP_SECURE', 'ssl');
+define('SMTP_PORT', 587);
+define('SMTP_SECURE', 'tls');
 define('SMTP_USER', 'hallo@werbeinsel.de');     // najčešće cela adresa
-define('SMTP_PASS', 'J&610523287761oq');     // lozinka od tog mailbox-a (ili app password)
+define('SMTP_PASS', 'T&244424030699on');     // lozinka od tog mailbox-a (ili app password)
 define('SMTP_FROM', 'hallo@werbeinsel.de');     // neka se poklapa sa SMTP_USER
 define('SMTP_FROM_NAME', 'Werbeinsel Kontakt');
 define('SMTP_TEST_TOKEN', 'unesi-neku-jaku-vrednost-ovde');
