cb0c89199e6b7e8ab5701860a9f29cc6abc23b1a
marko <marko3ecvetkovic@gmail.com>
2025-09-11 23:55:02 +0200

    SMTP: switch to Office365 settings2

--- wp-content/smtp-config.php
+++ wp-content/smtp-config.php
@@ -4,7 +4,7 @@ define('SMTP_HOST', 'smtp.office365.com');  // ili smtp.strato.de / email-smtp.<
 define('SMTP_PORT', 587);
 define('SMTP_SECURE', 'tls');
 define('SMTP_USER', 'hallo@werbeinsel.de');     // najčešće cela adresa
-define('SMTP_PASS', 'T&244424030699on');     // lozinka od tog mailbox-a (ili app password)
+define('SMTP_PASS', 'V!142213160639ag');     // lozinka od tog mailbox-a (ili app password)
 define('SMTP_FROM', 'hallo@werbeinsel.de');     // neka se poklapa sa SMTP_USER
 define('SMTP_FROM_NAME', 'Werbeinsel Kontakt');
 define('SMTP_TEST_TOKEN', 'unesi-neku-jaku-vrednost-ovde');
