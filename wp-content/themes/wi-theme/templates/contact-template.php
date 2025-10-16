72062c6cbc4a89bde49623385b42677c7665470d
marko <marko3ecvetkovic@gmail.com>
2025-10-16 20:15:20 +0200

    contact fix

--- wp-content/themes/wi-theme/templates/contact-template.php
+++ wp-content/themes/wi-theme/templates/contact-template.php
@@ -4,7 +4,7 @@
  */
 get_header();
 
-$next_url       = content_url('uploads/next-contact/index.html?v=19');
+$next_url       = content_url('uploads/next-contact/index.html?v=20');
 $form_html      = do_shortcode('[wi_contact_form]');
 $plugin_css_url = plugins_url('assets/front.css', WP_PLUGIN_DIR . '/wi-contact/wi-contact.php');
 
